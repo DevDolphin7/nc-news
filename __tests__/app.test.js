0fdb3dda702dcc9c35594ea550c2fd4236a1fb92
Dan Olver <devdolphin7@outlook.com>
2024-07-15 15:55:28 +0100

    GET /api/articles/:article_id tested and complete

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -28,7 +28,7 @@ describe("/api", () => {
           const routes = Object.keys(endpoints);
           expect(routes.length).toBeGreaterThan(0);
 
-          validRoute = /^(GET|PUT|POST|PATCH|DELETE){1} (\/[A-Za-z:=?]*)*$/;
+          validRoute = /^(GET|PUT|POST|PATCH|DELETE){1} (\/[A-Za-z_:=?]+)*$/;
           routes.forEach((route) => {
             expect(validRoute.test(route)).toBe(true);
           });
@@ -89,6 +89,73 @@ describe("/api/topics", () => {
             expect(typeof topic.description).toBe("string");
           });
         });
+    });
+  });
+});
+
+describe("/api/articles/:article_id", () => {
+  describe("GET", () => {
+    describe("Happy paths", () => {
+      test("Returns an object with a key: article", () => {
+        return request(app)
+          .get("/api/articles/2")
+          .expect(200)
+          .then(({ body }) => {
+            expect(body.article).toBeTruthy();
+          });
+      });
+
+      test("Article is an object with keys: author, title, article_id, body, topic, created_at, votes, article_img_url", () => {
+        return request(app)
+          .get("/api/articles/3")
+          .expect(200)
+          .then(({ body }) => {
+            const keys = Object.keys(body.article);
+            expect(keys).toHaveLength(8);
+
+            const allowedKeys = [
+              "author",
+              "title",
+              "article_id",
+              "body",
+              "topic",
+              "created_at",
+              "votes",
+              "article_img_url",
+            ];
+            keys.forEach((key) => expect(allowedKeys.includes(key)).toBe(true));
+          });
+      });
+
+      test("The returned object is the requested article (article_id = requested number", () => {
+        return request(app)
+          .get("/api/articles/4")
+          .expect(200)
+          .then(({ body }) => {
+            const article = body.article;
+            expect(article.article_id).toBe(4);
+          });
+      });
+    });
+
+    describe("Sad paths", () => {
+      test("400: Responds with 400 Bad Request on a non positive integer input", () => {
+        return request(app)
+          .get("/api/articles/dog")
+          .expect(400)
+          .then(({ body }) => {
+            expect(body.message).toBe("Bad request");
+          });
+      });
+
+      test("404: Responds with 404 Not Found on a valid input that doesn't exist", () => {
+        return request(app)
+          .get("/api/articles/888")
+          .expect(404)
+          .then(({ body }) => {
+            expect(body.message).toBe("Article not found")
+          });
+      });
     });
   });
 });
