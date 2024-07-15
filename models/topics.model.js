83ba18895c05e5d31134742b7d4069cbd437deb4
Dan Olver <devdolphin7@outlook.com>
2024-07-15 14:27:27 +0100

    Remove console.log

--- models/topics.model.js
+++ models/topics.model.js
@@ -2,7 +2,6 @@ const db = require("../db/connection");
 
 exports.fetchTopics = () => {
   return db.query("SELECT * FROM topics").then(({ rows }) => {
-    console.log(rows)
     return rows;
   });
 };
