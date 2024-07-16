32590b3d3378d76dd9275c023644e56bfda2c866
Dan Olver <devdolphin7@outlook.com>
2024-07-16 10:29:38 +0100

    GET /api/articles (no queries) tested and complete

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,5 +1,22 @@
 const db = require("../db/connection");
 
+exports.fetchArticles = () => {
+  return db
+    .query(
+      `SELECT author, title, article_id, topic, created_at,
+      votes, article_img_url, comment_count
+      FROM articles
+      LEFT JOIN
+      (SELECT COUNT(article_id) AS comment_count, article_id AS temp_id
+      FROM comments GROUP BY article_id) temp_table
+      ON articles.article_id = temp_table.temp_id
+      ORDER BY created_at DESC`
+    )
+    .then(({ rows }) => {
+      return rows;
+    });
+};
+
 exports.fetchArticleById = (id) => {
   return db
     .query("SELECT * FROM articles WHERE article_id=$1", [id])
